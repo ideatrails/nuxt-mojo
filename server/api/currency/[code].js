83156ba8172fc29d05b184bc01b3b7c68ad8331d
Ideas <IdeaTrails@gmail.com>
2023-03-13 16:32:31 -0300

    chcek fix (rebreak) production

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -9,7 +9,8 @@ export default defineEventHandler(async (event) => {
   // const { goods } = process.env.GOODNESS
   // console.log(`goodness : ${goods}`)
   const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
-  const { data } = await $fetch(uri, {force: "true"})
+  // const { data } = await $fetch(uri, {force: "true"})
+  const { data } = await $fetch(uri)
 
   return data
 })
